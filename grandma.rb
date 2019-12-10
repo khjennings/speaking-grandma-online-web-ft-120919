b48a31244bb1fbe0aa0abb35046b5e0e941349af
Khadijah Jennings <kjennings855@gmail.com>
2019-12-10 09:55:36 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+if ==".downcase"
+  puts "HUH?! SPEAK UP,SONNY!"
+elseif ==".upcase"
+  puts "NO, NOT SINCE 1938!"
+else =="I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
